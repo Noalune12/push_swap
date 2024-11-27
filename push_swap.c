3edb5afa57ea1323a11de0492910e6cb98928c8f
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-11-27 12:46:07 +0100

    structure def

---
+++ push_swap.c
@@ -0,0 +1,10 @@
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_node	stack_a;
+	t_node	stack_b;
+
+
+
+}
---
+++ push_swap.h
@@ -0,0 +1,12 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_node
+{
+	int	value;
+	void	*prev;
+	void	*next;
+}	t_node;
+
+
+#endif
